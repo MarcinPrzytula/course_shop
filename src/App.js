764f6c1d09ea19a5d9fedb92a9a0b4bcd8cd2d8c
Marcin <mprzytula94@gmail.com>
2021-10-19 09:08:59 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './styles/App.css';
 
 function App() {
-  return <div>Course Shop - Main Page</div>;
+  return (
+    <>
+      <div>Course Shop - Main Page</div>
+      <div>aaa</div>
+    </>
+  );
 }
 
 export default App;
