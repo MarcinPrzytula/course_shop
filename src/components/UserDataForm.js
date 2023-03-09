a7e5b02ad4e9fc2e0979db1209de1af9eda84617
Marcin <mprzytula94@gmail.com>
2023-03-09 10:41:34 +0100

    styled userDataForm

--- src/components/UserDataForm.js
+++ src/components/UserDataForm.js
@@ -1,4 +1,7 @@
 import { Formik, Field, Form, ErrorMessage } from 'formik';
+import '../styles/UserDataForm.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
 
 const UserDataForm = ({ handler, buttonName }) => {
   return (
@@ -26,36 +29,48 @@ const UserDataForm = ({ handler, buttonName }) => {
       }}
     >
       {({ errors, touched, values }) => (
-        <Form>
-          <div className="login">
-            {errors.login && touched.login ? (
-              <ErrorMessage name="login" component="div" />
-            ) : null}
-            <span>Login</span>
+        <Form className="userDataForm">
+          <div className="userDataForm__login">
+            <div className="userDataForm__error">
+              {errors.login && touched.login ? (
+                <ErrorMessage name="login" component="div" />
+              ) : null}
+            </div>
+
             <Field
-              className="registrationPage__input"
+              className="userDataForm__input"
               name="login"
-              placeholder="login"
               value={values.login
                 .replace(/\s/g, '')
                 .replace(/</g, '')
                 .replace(/>/g, '')
                 .trim()}
+              placeholder="Login"
             />
           </div>
-          <div className="password">
-            {errors.password && touched.password ? (
-              <ErrorMessage name="password" component="div" />
-            ) : null}
-            <span>Password</span>
-            <Field
-              className="registrationPage__input"
-              placeholder="password"
-              name="password"
-              type="password"
-            />
+
+          <div className="userDataForm__password">
+            <div className="userDataForm__error">
+              {' '}
+              {errors.password && touched.password ? (
+                <ErrorMessage name="password" component="div" />
+              ) : null}
+            </div>
+            <div className="userDataForm__password-input-container">
+              <Field
+                className="userDataForm__input"
+                placeholder="Password"
+                name="password"
+                type="password"
+              />
+              <FontAwesomeIcon
+                className="userDataForm__password userDataForm__password--lock-icon"
+                icon={faLock}
+              />
+            </div>
           </div>
-          <button type="submit" className="registrationPage__button">
+
+          <button type="submit" className="userDataForm__button">
             {buttonName}
           </button>
         </Form>
